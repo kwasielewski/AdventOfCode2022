69b956a56f8c0ffe3a3aaffee0483cc30e398810
krzychuwas <krzychuwas@gmail.com>
2022-12-02 15:48:42 +0100

    Day 1

---
+++ Day1/calories2.jl
@@ -0,0 +1,21 @@
+function f()
+	x = zeros(Int64, 0)
+	record = 0
+	sum = 0
+	for line in eachline("calories.txt")
+		if line == ""
+			append!(x, sum)
+			sum = 0
+		else
+			sum += parse(Int64, line)
+		end
+	end
+	append!(x, sum)
+	sort!(x)
+	idx = size(x)[1]
+	println(x[idx]+x[idx-1]+x[idx-2])
+end
+
+f()
+
+
