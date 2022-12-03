be027e8073ebdb2a6202344a58cdaea9ba3cadfb
krzychuwas <krzychuwas@gmail.com>
2022-12-03 09:55:22 +0100

    Day 3

---
+++ Day3/rucksack.jl
@@ -0,0 +1,32 @@
+function ltn(c::Char)
+	v = Int(c)
+	if v >= 97
+		return v - 97 + 1
+	else
+		return v - 65 + 1 + 26
+	end
+end
+function f()
+	
+	sum = 0
+
+	for line in eachline("rucksack.txt")
+		x = length(line)
+		x = x ÷ 2
+		y = zeros(Bool, 52)
+		for i in 1:x
+			y[ltn(line[i])] = true
+		end
+		for i in x+1:length(line)
+			if y[ltn(line[i])]
+				sum += ltn(line[i])
+                break
+			end
+		end
+	end
+	
+	println(sum)
+
+end
+
+f()
