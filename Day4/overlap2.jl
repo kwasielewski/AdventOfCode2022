a624d6c1c1221f64c67bcd533a738a0ad5347368
kwasielewski <73347579+kwasielewski@users.noreply.github.com>
2022-12-04 12:05:27 +0100

    Update overlap2.jl

--- Day4/overlap2.jl
+++ Day4/overlap2.jl
@@ -3,7 +3,7 @@ function f()
 	
   sum = 0
 
-	for line in eachline("./day4.txt")
+  for line in eachline("./day4.txt")
     r, a, b, c, d = @scanf(line, "%d-%d,%d-%d", Int, Int, Int, Int)
     #println(a, b, c, d)
     l = max(a, c)
@@ -11,7 +11,7 @@ function f()
     if l <=p 
       sum += 1
     end
-	end
+  end
 	
   println(sum)
 
